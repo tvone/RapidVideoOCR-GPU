d25e6b575f05df19459283438ecf2b0e310a14b9
duccuong.le <duccuong.le@wiinte.com>
2025-06-30 20:29:38 +0700

    update output format srt and path the model

--- onlyOCR.py
+++ onlyOCR.py
@@ -3,14 +3,15 @@ from rapidocr import EngineType, LangRec, ModelType, OCRVersion
 import glob
 import os
 
-
-model_v5 = "/content/drive/MyDrive/RapidVideoOCR/models/PP-OCRv5_server_rec_infer"
-txt_path = "/content/drive/MyDrive/RapidVideoOCR/models/PP-OCRv5_server_rec_infer/ppocrv5_dict.txt"
+model_v5 = "/content/RapidVideoOCR-GPU/models/PP-OCRv5_server_rec_infer"
+txt_path = "/content/RapidVideoOCR-GPU/models/PP-OCRv5_server_rec_infer/ppocrv5_dict.txt"
 # Document: https://rapidai.github.io/RapidOCRDocs/main/install_usage/rapidocr/usage/#__tabbed_3_4
 ocr_input_params = RapidVideOCRInput(
-   is_batch_rec=False,
-   ocr_params={
-        "Rec.model_dir": model_v5,
+    is_batch_rec=False,
+    out_format="srt",
+    # Document params: https://rapidai.github.io/RapidOCRDocs/main/install_usage/rapidocr/parameters/?h=rec+lang+type
+    ocr_params={
+        "Rec.model_dir": model_v5,  # model_dir for paddlepaddle-gpu, if it diffirent will be model_path
         "Rec.engine_type": EngineType.PADDLE,
         "Rec.lang_type": LangRec.JAPAN,
         "Rec.model_type": ModelType.SERVER,
@@ -18,7 +19,7 @@ ocr_input_params = RapidVideOCRInput(
         "EngineConfig.paddle.use_cuda": True,  # 使用PaddlePaddle GPU版推理
         "EngineConfig.paddle.gpu_id": 0,  # 指定GPU id
         "Rec.rec_keys_path": txt_path
-   }
+    }
 )
 # Version GPU for google colab
 extractor = RapidVideOCR(ocr_input_params)
@@ -34,6 +35,3 @@ for folder in glob.glob(os.path.join(rgb_dir, "*/")):
         print(f"\nProcessing directory: {folder}\n")
         # outputs/a.srt  outputs/a.txt
         extractor(folder, save_dir, save_name=folder_name)
-
-
-
