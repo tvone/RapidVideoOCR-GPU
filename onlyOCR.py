147332efa0e8182c18b00af96e0c13c8b77ba992
duccuong.le <duccuong.le@wiinte.com>
2025-07-08 23:44:05 +0700

    update Global.max_side_len

--- onlyOCR.py
+++ onlyOCR.py
@@ -75,8 +75,7 @@ for folder in glob.glob(os.path.join(rgb_dir, "*/")):
         with Image.open(image_files) as img:
             w, h = img.size  # h = crop_height (giả sử crop sub dọc)
 
-        size_image = int(target_scale * (batch_size * (h + padding)))
-        limit_side_len = size_image
+        limit_side_len = int(target_scale * (batch_size * (h + padding)))
         # Require multiples of 32
         if limit_side_len % 32 != 0:
             limit_side_len = ((limit_side_len // 32) + 1) * 32
@@ -84,7 +83,7 @@ for folder in glob.glob(os.path.join(rgb_dir, "*/")):
         # print(limit_side_len)
 
         folder_ocr_params = deepcopy(ocr_input_params.ocr_params)
-        folder_ocr_params["Det.limit_side_len"] = 736 if size_image < 736 else limit_side_len
+        folder_ocr_params["Det.limit_side_len"] = 736 if limit_side_len < 736 else limit_side_len
         # folder_ocr_params["Global.max_side_len"] = w * target_scale
         # print(folder_ocr_params)
 
