2de1a149e61b61b9dbf94f25440cf8d1d44195ec
duccuong.le <duccuong.le@wiinte.com>
2025-07-10 10:56:14 +0700

    update rgb_dir_list

--- onlyOCR.py
+++ onlyOCR.py
@@ -59,7 +59,7 @@ class OCR:
             }
         }
 
-        self.rgb_dir_list = ["images/1", "images2/2"]
+        self.rgb_dir_list = ["images/1", "images/2"]
         self.save_dir = "/content/drive/MyDrive/RapidVideoOCR/outputs" if self.linux else "./outputs"
         # Options
         self.target_scale = 2
